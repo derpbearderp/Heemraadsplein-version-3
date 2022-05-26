75aa6c5e257d28af713875fe34c0de74e5f40348
Joey-bergwerff <89838983+derpbearderp@users.noreply.github.com>
2022-05-26 13:08:18 +0200

    Add files via upload

---
+++ Frontend/pages/homepage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/homepage.css" type="text/css">
+
+
+
+</head>
+<body>
+
+<div class="row flex-container">
+    <div class="columnh" id="top-green" >
+        <h1>events</h1>
+    </div>
+    <div class="columnh" id="top-red">
+        <h1>gebeurtenissen</h1>
+    </div>
+    <div class="columnh" id="top-blue">
+        <h1>hulp</h1>
+    </div>
+</div>
+
+<div class="row flex-container">
+    <div class="column" id="green">
+    </div>
+    <div class="column" id="red">
+    </div>
+    <div class="column" id="blue">
+    </div>
+</div>
+
+</body>
+</html>
